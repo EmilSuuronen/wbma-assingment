1d51fe5252f38a0e95060e3b306a3830931ac532
Emil <emilsuuronen@gmail.com>
2023-01-11 09:47:25 +0200

    file changes

--- App.js
+++ App.js
@@ -1,14 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, SafeAreaView} from 'react-native';
+import List from "./SafeAreaView/List/List";
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <SafeAreaView style={styles.container}>
+        <List />
+      </SafeAreaView>
+      <StatusBar style="auto"/>
+    </>
   );
-}
+};
+
 
 const styles = StyleSheet.create({
   container: {
@@ -18,3 +22,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App;
